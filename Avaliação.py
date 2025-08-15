9282aecef53391b4f84bcd10ba78fb919e0dc180
HELISSON SILVA SOUSA <helissonsilvasousa94@gmail.com>
2025-08-15 14:54:23 -0300

    Add files via upload

---
+++ Avaliação.py
@@ -0,0 +1,42 @@
+#print("Olá mundo!")
+
+#l1=input('Qual e seu nome?: ')
+
+#print(f'Seja bem Vindo {l1}! e um Prazer te conhecer!')
+
+#l1 = input("Nome do funcionario: ")
+#s1 = input("Salário:")
+
+#print(f"O Funcionário {l1} tem um salário de {s1} em Julho.")
+
+#n1 = int(input('Digite um numero: '))
+#n2 = int(input('Digite outro numero para somar ao primeiro: '))
+
+#print(f'A soma de {n1} e {n2} é igual a {n1+n2}')
+
+# Pedindo as notas
+#port = float( input("Nota 1: ") )
+#ing = float( input(" Nota 2: ") )
+
+# Cálculo da média
+#media = port + ing / 2
+
+#print("A média desse estudante foi: ",media)
+
+#n1 = int(input('Digite um numero: '))
+
+#print(f'O numero antercesor de {n1} e {n1-1} e o sucessor {n1+1}')
+
+#n1 = int(input('Digite um numero: '))
+
+#print(f'O dobro de {n1} e {n1*2} e sua terça parte é {n1//3}')
+
+ms = float(input("Digite uma distância em metros: "))
+km = ms / 1000
+hm = ms / 100
+dam = ms / 10
+dm = ms * 10
+cm = ms * 100
+mm = ms * 1000
+print(f"O valor de {ms} metros é igual a {km} Km, {hm} Hm, {dam} Dam, {dm} dm, {cm} cm, {mm} mm")
+	
